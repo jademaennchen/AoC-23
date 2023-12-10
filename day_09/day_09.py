f26c9df1b91057f32bbd12a42c1028f2ac120df1
jademaennchen <nicolas.beninde@gmail.com>
2023-12-10 23:21:40 +0100

    Day_10

--- day_09/day_09.py
+++ day_09/day_09.py
@@ -14,17 +14,18 @@ def input_from_file(file):
 
 #part_1
 #part_2
-def calc_sum_of_extrapolated_values(histories, prev):
+def calc_sum_of_extrapolated_values(histories, part2):
     running_sum = 0
     for history in histories:
         while not all(value == 0 for value in history[-1]):
             history.append([x - y for x, y in zip(history[-1][1:], history[-1])])
-        if prev: running_sum += sum([layer[0] if i % 2 == 0 else -layer[0] for i, layer in enumerate(history)])
-        else: running_sum += sum([layer[-1] for layer in history])
+        if not part2: running_sum += sum([layer[-1] for layer in history])
+        else: running_sum += sum([layer[0] if i % 2 == 0 else -layer[0] for i, layer in enumerate(history)])
     return running_sum
 
 
 #main
 if __name__ == '__main__':
     histories = input_from_file("day_09/input.txt")
-    print(calc_sum_of_extrapolated_values(histories))
+    print(calc_sum_of_extrapolated_values(histories, False))
+    print(calc_sum_of_extrapolated_values(histories, True))
